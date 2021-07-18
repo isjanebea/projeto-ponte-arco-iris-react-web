139b676f05c627ed01524e9b7118740f065b34fc
Beatriz-Ramerindo <janeramerindo@gmail.com>
2021-07-18 12:09:22 -0300

    ajustes nas cores do tema

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -9,15 +9,16 @@ const Dark = {
     bgImage: "rgba(0,0,0,.8)",
     bgOpacity : "0,0,0",
     bordersecondary : '0,0,0',
+    bgContrast: "45,45,45",
     bgCard : "rgb(255,240,255)",
     bgCard2 : "rgba(255,255,255,0.8)",
     borderCard : "rgba(30,30,30,0.2)",
     cardColor : "rgb(60,0,30)",
-    bg : "rgb(20,20,20)",
-    filterBg : "rgba(255,255,255,0.3)"
+    bg : "rgb(30,30,30)",
+    filterBg : "rgba(255,255,255,0.3)",
+    inputBg: "rgb(40,40,40)",
+    inputDisabled: "rgb(50,50,50)",
 }
-
-
 // const Light = {
 //     primary : "#EB0C6B",
 //     secondary: "white",
@@ -28,7 +29,12 @@ const Dark = {
 // }
 const Light = {
     ...Dark,
-    primary : "red"
+    primary : "rgb(30,30,30)",
+    secondary: "rgb(40,0,0)", 
+    bg : "rgb(230,230,230)",
+    bgContrast: "255,255,255",
+    inputBg: "rgb(230,230,230)",
+    inputDisabled: "rgb(250,250,250)",
 }
 
 
@@ -46,4 +52,6 @@ class Theme {
     
 }
 
-export default new Theme();
+const theme = new Theme();
+
+export default theme;
